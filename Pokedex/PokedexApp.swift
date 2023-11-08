bffefc37e5b3b6fff726b6149e5aee8bb9e62029
Suguru Tokuda <suguru.tokuda@gmail.com>
2023-11-08 13:49:24 -0500

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Suguru Tokuda on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
